ae7691ab0423fcef5f5f7a5a2cabfead603792d8
Lenoir Zamboni <lenoirzamboni@gmail.com>
2015-11-22 18:57:19 -0200

    Adiciona a classe InformarPerdedor

---
+++ TicTackle/src/dialogs/InformarPerdedor.java
@@ -0,0 +1,55 @@
+package dialogs;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+public class InformarPerdedor extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private final Border RAISED_LEVEL = BorderFactory.createRaisedBevelBorder();
+	private final Border LOWERED_LEVEL = BorderFactory
+			.createLoweredBevelBorder();
+	private final CompoundBorder BORDER_COMPOUND = BorderFactory
+			.createCompoundBorder(RAISED_LEVEL, LOWERED_LEVEL);
+	
+	/**
+	 * Create the dialog.
+	 */
+	public InformarPerdedor() {
+		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		this.setVisible(true);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setBorder(BORDER_COMPOUND);
+		contentPanel.setLayout(null);
+		
+		JLabel lblOJogadorTal = new JLabel("Vc perdeu a partida! :(");
+		lblOJogadorTal.setFont(new Font("Lithos Pro", Font.PLAIN, 20));
+		lblOJogadorTal.setBounds(41, 92, 368, 54);
+		contentPanel.add(lblOJogadorTal);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+}
