360c2f441f0c220ac731339c2a888650edec9547
Lenoir Zamboni <lenoirzamboni@gmail.com>
2015-11-22 19:17:43 -0200

    Adiciona a responsabilidade de realizar lance diretamente para a classe tabuleiro (classe atorJogador foi excluída)
    Adiciona a responsabilidade de desistência da partida:
    métodos desistirPartida() e confirmarDesistencia()
    
    Renomeia disposeInterface para fecharImagemTabuleiro
    
    Adiciona o tratamento de conexão para vários casos
    
    Adiciona métodos, mostrarErroConexao(), iniciar()(pré-smart do jogo)
    criarJogo()(criar a imagem do tabuleiro), mostraDaVezTabuleiro(),
    partidaCancelada(), informarPerdedor(), informarVencedor() e 
    fimDePartida()

--- TicTackle/src/controller/Controle.java
+++ TicTackle/src/controller/Controle.java
@@ -1,14 +1,11 @@
 package controller;
 
-import java.awt.Color;
-
-import view.ComecarPartida;
-import view.ComoJogar;
-import view.Conectar;
+import dialogs.ComoJogar;
+import dialogs.Mensagens;
 import view.ImagemTabuleiro;
-import model.AtorJogador;
 import model.AtorNetGames;
 import model.Jogador;
+import model.Lance;
 import model.Posicao;
 import model.Tabuleiro;
 
@@ -17,38 +14,53 @@ public class Controle {
 	private Tabuleiro mTabuleiro;
 	private ImagemTabuleiro mImagemTabuleiro;
 	private AtorNetGames mAtorNetGames;
-	private AtorJogador mAtorJogador;
-	private Conectar mConectar;
 	private String nomeJogador;
+	private Mensagens mMensagens;
+	private Jogador mJogador;
 
 	public Controle(){
 		mAtorNetGames = new AtorNetGames(this);
-		mConectar = new Conectar(this);
+		mMensagens = new Mensagens(this);
+		mMensagens.mostrarConectar();
 	}
 
+	// ========================================REALIZAR_LANCE========================================//
+
+	
 	public boolean realizarLanceControle(Posicao prePos, Posicao posPos){
-		return mAtorJogador.realizarLanceJogador(prePos, posPos);
+		return mTabuleiro.realizarLanceTabuleiro(prePos, posPos);
 	}
 	
 	public void getPosicoesGame(Posicao [] posicoes){
 		mTabuleiro.getPosicoes(posicoes);
 	}
 	
+	// ========================================DESISTIR_PARTIDA========================================//
+	
 	public void desistirPartida(){
-		mAtorJogador.desistirPartida();
+		mMensagens.mostrarDesistirPartida();
 	}
 	
-	public void reiniciarPartida(){
-		mAtorJogador.reiniciarPartida();
+	public void confirmarDesitencia() {
+		Lance lance = new Lance();
+		lance.setPartidaDesistida(true);
+		mAtorNetGames.enviarJogada(lance);
+		
+		fimDePartida();
 	}
+	
 
+	// ========================================COMO_JOGAR========================================//
+	
 	public void comoJogar(){
 		new ComoJogar();
 	}	
 	
-	public void disposeInterface(){
+	public void fecharImagemTabuleiro(){
 		mImagemTabuleiro.dismiss();
 	}
+	
+	// ========================================CONEXAO_SERVIDOR========================================//
 	
 	public void conectarComServidor(String server, String nome){
 		
@@ -57,36 +69,93 @@ public class Controle {
 		int resultado = mAtorNetGames.conectar(server, nomeJogador);
 		
 		switch (resultado) {
-		case 200:
-			mConectar.dismiss();
-			new ComecarPartida(this);
-			break;
 			
-		default:
-			mConectar.showError();
-			break;
+		case 200:
+				
+				switch (mAtorNetGames.iniciarPartida()) {
+				
+				case 200:
+					mMensagens.mostrarEspera();
+					iniciar();
+					break;
+
+				case 404:
+					mMensagens.mostrarErroConectar();
+					break;
+				}
+				break;
+				
+			case 201:
+				mMensagens.mostrarConexaoJaEstabelecida();
+				break;
+				
+			case 404:
+				mMensagens.mostrarErroConectar();
+				break;
+				
+			case 500:
+				mMensagens.mostrarErroPropriedades();
+				break;
 		}
 	}
 	
-	public void criarJogo(){
-		mImagemTabuleiro = new ImagemTabuleiro(this);
-		Jogador jogador;
+	public void mostrarErroConexao(){
+		mMensagens.mostrarErroConectar();
+	}
+	
+	// ========================================INICIAR/CRIAR_JOGO========================================//
+	
+	public void iniciar(){
+		
 		if(mAtorNetGames.temAdversario()){
-			jogador = new Jogador(nomeJogador, "vermelho");
-			mTabuleiro = new Tabuleiro(jogador, mAtorNetGames, this);
-			mTabuleiro.setDaVezJogador(false);
-			
+			mJogador = new Jogador(nomeJogador, "vermelho");
+			mTabuleiro = new Tabuleiro(mJogador, mAtorNetGames, this);
+			mTabuleiro.setDaVezJogador(false);			
 		}else{
-			jogador = new Jogador(nomeJogador, "azul");
-			mTabuleiro = new Tabuleiro(jogador, mAtorNetGames, this);
+			mJogador = new Jogador(nomeJogador, "azul");
+			mTabuleiro = new Tabuleiro(mJogador, mAtorNetGames, this);
 			mTabuleiro.setDaVezJogador(true);
 		}
 		
 		mAtorNetGames.setTabuleiro(mTabuleiro);
-		mAtorJogador = new AtorJogador(mTabuleiro);
 	}
 	
-	public void mostraDaVezTabuleiro(boolean v){
-		//mImagemTabuleiro.mostraDaVezTabuleiro(v);
+	public void criarJogo(){
+		new ImagemTabuleiro(this, mJogador.getCor());
+	}
+	
+	// ========================================MOSTRAR_DA_VEZ========================================//
+	
+	public void mostraDaVezTabuleiro(){
+		mImagemTabuleiro.mostraDaVezTabuleiro(mJogador.getCor());
+	}
+	
+	// ========================================PARTIDA_CANCELADA========================================//
+
+	public void partidaCancelada() {
+		mMensagens.mostrarPartidaCancelada();
+		fimDePartida();
+	}
+
+	// ========================================INFORMAR_PERDEDOR_VENCEDOR========================================//
+	
+	public void informarPerdedor() {
+		mMensagens.mostrarPerdedor();
+		
+	}
+
+	public void informarVencedor() {
+		mMensagens.mostrarVencedor();
+	}
+	
+	
+	// ========================================FIM_PARTIDA========================================//
+
+	public void fimDePartida() {
+		
+		fecharImagemTabuleiro();
+		mMensagens.resetConectar();
+		mMensagens.mostrarConectar();
+		
 	}
 }
