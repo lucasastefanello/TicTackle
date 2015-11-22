3222d71c096623623a938741fcb4a6ec6b907ac4
Lenoir Zamboni <lenoirzamboni@gmail.com>
2015-11-22 18:58:36 -0200

    Move a classe PartidaCancelada para o pacote dialogs

---
+++ TicTackle/src/dialogs/PartidaCancelada.java
@@ -0,0 +1,64 @@
+package dialogs;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class PartidaCancelada extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JButton okButton;
+
+	/**
+	 * Create the dialog.
+	 */
+	public PartidaCancelada() {
+		setPanel();
+		setListeners();
+	}
+	
+	public void setPanel(){
+		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		this.setVisible(true);
+		setBounds(100, 100, 531, 232);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+
+		JLabel lblPartidaCanceladaPelo = new JLabel(
+				"Partida cancelada pelo oponente");
+		lblPartidaCanceladaPelo.setFont(new Font("Lithos Pro", Font.PLAIN, 20));
+		lblPartidaCanceladaPelo.setBounds(54, 64, 423, 43);
+		contentPanel.add(lblPartidaCanceladaPelo);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+		okButton = new JButton("OK");
+		
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+
+	}
+	
+	public void setListeners(){
+		okButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+		});
+	}
+
+}
